3e4e0bf57d6bb26a8e77836f65469c504a0826e3
Juri Adams <juri@adams.sh>
2020-06-29 13:54:30 +0200

    📦 Updated output path and created first bundle

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     },
     output: {
         filename: "achorn.js",
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "lib"),
         libraryTarget: "var",
         library: "Achorn",
     },
